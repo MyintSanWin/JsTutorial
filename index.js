f8f30a8f89fe970359849338557a394680bbf033
MyintSanWin <jackmee24680@gmail.com>
2021-09-08 21:53:59 +0630

    rendering an error message

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-let saveEl = document.getElementById('save-el')
-let countEl = document.getElementById('count-el')
-let count =0;
-function increment(){
-    count += 1;
-    countEl.textContent =count;
-}
-
-function save(){
-
-    let countStr = count + ' - '
-    saveEl.textContent += " " + countStr
-    countEl.textContent = 0
-    count =0
+let errorParagraph = document.getElementById('error')
+console.log(errorParagraph)
+function purchase(){
+    console.log("button clicked")
+    errorParagraph.textContent = "Something went wrong, please try again"
 }
