ad5fa78d74e0afcdc22b035fb874f8d7619dd2a3
MyintSanWin <jackmee24680@gmail.com>
2021-09-10 16:27:29 +0630

    Blackjack start game and new card

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-let errorParagraph = document.getElementById('error')
-console.log(errorParagraph)
-function purchase(){
-    console.log("button clicked")
-    errorParagraph.textContent = "Something went wrong, please try again"
+let firstCard =12
+let secondCard=9
+let cards =[firstCard, secondCard]//array  ordered list of item
+let sum =firstCard+secondCard;
+let hasBlackJack=false
+let isAlive =true
+
+let messageEl=document.getElementById("message-el")
+let sumEl =document.querySelector('#sum-el')
+let cardsEl = document.getElementById('card-el')
+
+function startGame(){
+    renderGame()
+}
+
+function renderGame(){
+    //render out first card and second card
+cardsEl.textContent="Cards: " + cards[0] + " " + cards[1]
+
+//render out all the cards we have
+sumEl.textContent ="Sum: " + sum
+if(sum <= 20 ){
+    message="Do u want to draw a new card?"
+}
+else if(sum === 21){
+message="wohoo! You've got Blackjack!"
+}
+else {
+message="You're out of the game!"
+isAlive=false
+}
+messageEl.textContent=message
+}
+function newCard(){
+    // console.log("Drawing a new card from the deck!")
+    let card = 7
+    sum += card
+
+    renderGame()
 }
